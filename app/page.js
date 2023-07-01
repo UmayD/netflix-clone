750acb5e7ba00eb42cce85babdce0596d265f977
Umay <umayd96@gmail.com>
2023-07-02 02:26:21 +0300

    fetch movies, add poster, title and desc

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./globals.css";
+import MainContent from "@/components/MainContent";
 
 export default function Home() {
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <MainContent />
     </main>
   )
 }
---
+++ src/utils/MovieRequests.js
@@ -0,0 +1,8 @@
+const key = `108607d30fbf6a4090e4bf6b7d0d9454`
+
+const movieRequest = {
+    requestPopular: "https://api.themoviedb.org/3/movie/popular?api_key=108607d30fbf6a4090e4bf6b7d0d9454",
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}`,
+}
+
+export default movieRequest;
