e2a63ebe55eada76279eb986adb23638468f343c
Umay <umayd96@gmail.com>
2023-07-01 17:46:43 +0300

    add navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import React from "react";
+import "./globals.css";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
     </main>
   )
 }
