4cdcbab957ba60f402e4e9b5dc4558a5733b6ffe
Umay <umayd96@gmail.com>
2023-07-09 16:00:52 +0300

    add functional row sliders

--- components/Row.jsx
+++ components/Row.jsx
@@ -1,49 +1,52 @@
 "use client";
-import React, { useState } from "react";
-import FavoriteIcon from "@mui/icons-material/Favorite";
-import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import React from "react";
 import useSWR from "swr";
+import Movie from "./Movie";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const { data, error } = useSWR(fetchURL, fetcher);
-
-  const [like, setLike] = useState(false);
 
   if (error) return <div className="text-white">Failed to load</div>;
   if (!data) return <div className="text-white">Loading...</div>;
 
   const movies = data.results;
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
-      <div className="relative flex items-center">
-        <div id={"slider"}>
+      <div className="relative flex items-center group">
+        <NavigateBeforeIcon
+          onClick={slideLeft}
+          className="bg-white rounded-xl left-2 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 invisible group-hover:visible"
+          sx={{ width: "2.5%", height: "30%" }}
+        />
+        <div
+          id={"slider" + rowID}
+          className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
           {movies.map((item, id) => {
-            return (
-              <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500${item?.backdrop_path}`}
-                  alt={item?.title}
-                />
-                <div className="absolute left-0 top-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100">
-                  <p>
-                    {like ? (
-                      <FavoriteIcon className="absolute top-4 left-4 text-gray-300" />
-                    ) : (
-                      <FavoriteBorderIcon className="absolute top-4 left-4 text-gray-300" />
-                    )}
-                  </p>
-                  <p className="text-white md:text-sm whitespace-normal font-bold flex justify-center items-center h-full text-center">
-                    {item?.title}
-                  </p>
-                </div>
-              </div>
-            );
+            return <Movie key={id} item={item} />;
           })}
         </div>
+        <NavigateNextIcon
+          onClick={slideRight}
+          className="bg-white rounded-xl right-2 absolute opacity-50 hover:opacity-100 cursor-pointer z-10 invisible group-hover:visible"
+          sx={{ width: "2.5%", height: "30%" }}
+        />
       </div>
     </>
   );
