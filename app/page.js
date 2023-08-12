f2fba51fcfb591e3a33b9cf5dc569707e096558a
Umay <umayd96@gmail.com>
2023-08-12 23:43:41 +0300

    add protection

--- app/page.js
+++ app/page.js
@@ -3,14 +3,15 @@ import "./globals.css";
 import MainContent from "@/components/MainContent";
 import Row from "@/components/Row";
 import movieRequest from "@/src/utils/MovieRequests";
+import ProtectedRoute from "@/components/ProtectedRoute";
 
 export default function Home() {
 
   return (
-    <>
+    <ProtectedRoute>
       <MainContent />
       <Row rowID={1} title={"Popular"} fetchURL={movieRequest.requestPopular} />
       <Row rowID={2} title={"Top Rated"} fetchURL={movieRequest.requestTopRated} />
-    </>
+    </ProtectedRoute>
   )
 }
