2f1dc77385859df9cefb3b90c35f3e92d73f22ce
Umay <umayd96@gmail.com>
2023-08-12 23:44:36 +0300

    add logout props

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,24 +1,54 @@
+"use client";
 import React from "react";
 import Link from "next/link";
+import { UserAuth } from "@/src/context/AuthContext";
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
+  const { user, logOut } = UserAuth();
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      await logOut();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="flex items-center justify-between px-6 py-4 z-[100] w-full absolute">
       <Link href={"/"}>
         <h1 className="text-netred text-4xl font-bold">NETFLIX</h1>
       </Link>
 
-      <div className="flex justify-between items-center">
-        <Link href={"/signup"}>
-          <button className="text-white pr-4">Sign Up</button>
-        </Link>
+      {user?.email ? (
+        <div className="flex justify-between items-center">
+          <Link href={"/account"}>
+            <button className="text-white pr-4">Account</button>
+          </Link>
 
-        <Link href={"/login"}>
-          <button className="bg-netred px-4 py-1 rounded cursor-pointer text-white">
-            Sign In
-          </button>
-        </Link>
-      </div>
+          <Link href={"/"}>
+            <button onClick={handleLogout} className="bg-netred px-4 py-1 rounded cursor-pointer text-white">
+              Logout
+            </button>
+          </Link>
+        </div>
+      ) : (
+        <div className="flex justify-between items-center">
+          <Link href={"/signup"}>
+            <button className="text-white pr-4">Sign Up</button>
+          </Link>
+
+          <Link href={"/login"}>
+            <button className="bg-netred px-4 py-1 rounded cursor-pointer text-white">
+              Sign In
+            </button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
