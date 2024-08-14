b3c0c57f4c704ef8c22a2b32ad6d9dc247abfda8
Umay <umayd96@gmail.com>
2024-08-14 11:48:22 +0300

    correct api key

--- app/firebase.js
+++ app/firebase.js
@@ -2,17 +2,25 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import {
+  REACT_APP_FIREBASE_API_KEY,
+  REACT_APP_FIREBASE_AUTH_DOMAIN,
+  REACT_APP_FIREBASE_PROJECT_ID,
+  REACT_APP_FIREBASE_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_SENDER,
+  REACT_APP_ID,
+} from "@/.env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAEnnSPGyLS8NuHvhI_TuqefHY1g9xZozM",
-  authDomain: "netflix-nextjs-clone-24b44.firebaseapp.com",
-  projectId: "netflix-nextjs-clone-24b44",
-  storageBucket: "netflix-nextjs-clone-24b44.appspot.com",
-  messagingSenderId: "787035282939",
-  appId: "1:787035282939:web:f4ec0b5c29218afe86ff5e"
+  apiKey: `${REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${REACT_APP_FIREBASE_AUTH_DOMAIN}`,
+  projectId: `${REACT_APP_FIREBASE_PROJECT_ID}`,
+  storageBucket: `${REACT_APP_FIREBASE_STORAGE_BUCKET}`,
+  messagingSenderId: `${REACT_APP_MESSAGING_SENDER}`,
+  appId: `${REACT_APP_ID}`,
 };
 
 export const app = initializeApp(firebaseConfig);
